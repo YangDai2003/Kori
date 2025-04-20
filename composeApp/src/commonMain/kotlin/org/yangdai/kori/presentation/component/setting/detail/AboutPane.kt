c3cc5b217687f5e90d5a8552a5d5fced6e857cb8
Yang <dy15800837435@gmail.com>
2025-04-20 08:56:49 +0200

    init

--- composeApp/src/commonMain/kotlin/org/yangdai/kori/presentation/component/setting/detail/AboutPane.kt
+++ composeApp/src/commonMain/kotlin/org/yangdai/kori/presentation/component/setting/detail/AboutPane.kt
@@ -3,6 +3,7 @@ package org.yangdai.kori.presentation.component.setting.detail
 import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -13,7 +14,13 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.BugReport
+import androidx.compose.material3.Icon
+import androidx.compose.material3.ListItem
+import androidx.compose.material3.ListItemDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -25,16 +32,19 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.hapticfeedback.HapticFeedbackType
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.platform.LocalHapticFeedback
+import androidx.compose.ui.platform.LocalUriHandler
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import kori.composeapp.generated.resources.Res
 import kori.composeapp.generated.resources.app_name
 import kori.composeapp.generated.resources.compose_multiplatform
+import kori.composeapp.generated.resources.report_a_bug_or_request_a_feature
 import kori.composeapp.generated.resources.version
 import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.resources.stringResource
@@ -43,12 +53,13 @@ import org.yangdai.kori.presentation.component.CurlyCornerShape
 
 @Composable
 fun AboutPane() {
+    val uriHandler = LocalUriHandler.current
     var showConfetti by rememberSaveable { mutableStateOf(false) }
 
     Column(
         Modifier
             .fillMaxSize()
-            .padding(horizontal = 8.dp)
+            .padding(horizontal = 16.dp)
             .verticalScroll(rememberScrollState()),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
@@ -117,6 +128,21 @@ fun AboutPane() {
         }
 
         Spacer(modifier = Modifier.height(16.dp))
+
+        ListItem(
+            modifier = Modifier
+                .padding(bottom = 8.dp)
+                .clip(CircleShape)
+                .clickable {
+                    uriHandler.openUri("https://github.com/YangDai2003/Kori")
+                },
+            colors = ListItemDefaults.colors(containerColor = MaterialTheme.colorScheme.surfaceContainerLowest),
+            leadingContent = {
+                Icon(imageVector = Icons.Outlined.BugReport, contentDescription = null)
+            },
+            headlineContent = {
+                Text(text = stringResource(Res.string.report_a_bug_or_request_a_feature))
+            })
     }
 
     if (showConfetti) {
