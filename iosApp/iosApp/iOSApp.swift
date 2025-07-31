a64aa60a0353858ce8041316455df742862bf8ea
yang <dy15800837435@gmail.com>
2025-07-31 17:08:07 +0200

    init

--- iosApp/iosApp/iOSApp.swift
+++ iosApp/iosApp/iOSApp.swift
@@ -1,10 +1,27 @@
 import SwiftUI
+import ComposeApp
 
 @main
 struct iOSApp: App {
+
+    // Register the AppDelegate to handle incoming URLs
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
+    }
+}
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(
+        _ application: UIApplication,
+        open uri: URL,
+        options: [UIApplication.OpenURLOptionsKey: Any] = [:]
+    ) -> Bool {
+        // Sends the full URI on to the singleton
+        ExternalUriHandler.shared.onNewUri(uri: uri.absoluteString)
+        return true
     }
 }
