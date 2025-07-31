9e36f9e229309802bea841a6d131c44c4999d086
yang <dy15800837435@gmail.com>
2025-07-31 17:00:40 +0200

    init

--- iosApp/iosApp/ContentView.swift
+++ iosApp/iosApp/ContentView.swift
@@ -15,13 +15,3 @@ struct ContentView: View {
         ComposeView().ignoresSafeArea() // Compose has own keyboard handler
     }
 }
-
-func application(
-    _ application: UIApplication,
-    open uri: URL,
-    options: [UIApplication.OpenURLOptionsKey: Any] = [:]
-) -> Bool {
-    // Sends the full URI on to the singleton
-    ExternalUriHandler.shared.onNewUri(uri: uri.absoluteString)
-        return true
-    }
