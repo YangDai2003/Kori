38c6d1050aae018311e0c0138c331a5031d0c299
Yang <dy15800837435@gmail.com>
2025-08-05 07:32:38 +0200

    init

--- composeApp/src/iosMain/kotlin/org/yangdai/kori/presentation/component/main/ToolboxPage.ios.kt
+++ composeApp/src/iosMain/kotlin/org/yangdai/kori/presentation/component/main/ToolboxPage.ios.kt
@@ -1,6 +1,17 @@
 package org.yangdai.kori.presentation.component.main
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.outlined.ArrowRight
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.ListItem
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.unit.dp
 
 @Composable
 actual fun WidgetListItem() {
@@ -8,4 +19,20 @@ actual fun WidgetListItem() {
 
 @Composable
 actual fun InkListItem() {
+    ListItem(
+        modifier = Modifier
+            .padding(bottom = 8.dp)
+            .clip(CardDefaults.shape)
+            .clickable {
+                // TODO: Handle click
+            },
+        headlineContent = { Text("Ink Playground") },
+        supportingContent = { Text("This is an experimental feature and may be changed, presented in a different form, or even removed entirely.") },
+        trailingContent = {
+            Icon(
+                imageVector = Icons.AutoMirrored.Outlined.ArrowRight,
+                contentDescription = null
+            )
+        }
+    )
 }
