1e68c92f82fc34f80e7b2f731590abe39d85f994
yang <dy15800837435@gmail.com>
2025-08-17 00:58:02 +0200

    init

--- composeApp/src/iosMain/kotlin/org/yangdai/kori/presentation/component/note/drawing/DrawPreview.ios.kt
+++ composeApp/src/iosMain/kotlin/org/yangdai/kori/presentation/component/note/drawing/DrawPreview.ios.kt
@@ -2,14 +2,17 @@ package org.yangdai.kori.presentation.component.note.drawing
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.viewinterop.UIKitView
+import org.yangdai.kori.presentation.util.toUIColor
 import platform.Foundation.NSFileManager
 import platform.Foundation.NSURL
 import platform.UIKit.UIImage
 import platform.UIKit.UIImageView
+import platform.UIKit.UIViewContentMode
 
 @Composable
 actual fun InNoteDrawPreview(uuid: String, imageBitmap: ImageBitmap?, modifier: Modifier) {
@@ -20,13 +23,16 @@ actual fun InNoteDrawPreview(uuid: String, imageBitmap: ImageBitmap?, modifier: 
         ).first() as NSURL
         val imagePath = documentDirectory.URLByAppendingPathComponent("$uuid/ink.png")?.path
         val image = imagePath?.let { UIImage.imageWithContentsOfFile(it) }
+        val backgroundColor = MaterialTheme.colorScheme.background
         if (image != null)
             UIKitView(
                 modifier = modifier,
                 factory = {
-                    val imageView = UIImageView()
-                    imageView.image = image
-                    imageView
+                    UIImageView().apply {
+                        this.image = image
+                        contentMode = UIViewContentMode.UIViewContentModeScaleAspectFit
+                        this.backgroundColor = backgroundColor.toUIColor()
+                    }
                 }
             )
         else Spacer(modifier)
