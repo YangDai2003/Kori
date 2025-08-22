a45d921c3378eb0b1fac34a77bad22f64fc12000
yang <dy15800837435@gmail.com>
2025-08-22 18:12:50 +0200

    init

--- composeApp/src/commonMain/kotlin/org/yangdai/kori/presentation/screen/settings/SettingsScreen.kt
+++ composeApp/src/commonMain/kotlin/org/yangdai/kori/presentation/screen/settings/SettingsScreen.kt
@@ -10,7 +10,6 @@ import androidx.compose.animation.slideInVertically
 import androidx.compose.animation.slideOutVertically
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.gestures.detectVerticalDragGestures
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -19,12 +18,15 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.filled.ArrowBackIosNew
 import androidx.compose.material.icons.filled.Close
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.adaptive.ExperimentalMaterial3AdaptiveApi
 import androidx.compose.material3.adaptive.currentWindowAdaptiveInfo
 import androidx.compose.material3.adaptive.layout.AnimatedPane
@@ -42,7 +44,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.backhandler.BackHandler
@@ -74,7 +75,7 @@ import kotlin.coroutines.cancellation.CancellationException
 
 @OptIn(
     ExperimentalMaterial3AdaptiveApi::class, ExperimentalMaterial3ExpressiveApi::class,
-    ExperimentalComposeUiApi::class
+    ExperimentalComposeUiApi::class, ExperimentalMaterial3Api::class
 )
 @Composable
 fun SettingsScreen(navigateUp: () -> Unit) {
@@ -158,8 +159,8 @@ fun SettingsScreen(navigateUp: () -> Unit) {
             border = BorderStroke(width = 1.dp, color = MaterialTheme.colorScheme.outlineVariant)
         ) {
             Column {
-                Box(
-                    Modifier.fillMaxWidth().padding(2.dp).pointerInput(Unit) {
+                CenterAlignedTopAppBar(
+                    modifier = Modifier.fillMaxWidth().pointerInput(Unit) {
                         detectVerticalDragGestures(
                             onDragEnd = {
                                 coroutineScope.launch {
@@ -188,60 +189,58 @@ fun SettingsScreen(navigateUp: () -> Unit) {
                                 backProgress.snapTo(newProgress)
                             }
                         }
-                    }
-                ) {
-                    if (navigator.canNavigateBack()) {
-                        IconButton(
-                            modifier = Modifier.align(Alignment.CenterStart),
-                            shape = dialogShape(),
-                            onClick = {
-                                coroutineScope.launch {
-                                    navigator.navigateBack()
+                    },
+                    expandedHeight = 52.dp,
+                    colors = TopAppBarDefaults.topAppBarColors(
+                        containerColor = MaterialTheme.colorScheme.surfaceContainer,
+                        navigationIconContentColor = MaterialTheme.colorScheme.onSurface,
+                        actionIconContentColor = MaterialTheme.colorScheme.onSurface,
+                    ),
+                    title = {
+                        Text(
+                            if (isExpanded)
+                                stringResource(Res.string.settings)
+                            else
+                                when (selectedItem) {
+                                    0 -> stringResource(Res.string.style)
+                                    1 -> stringResource(Res.string.editor)
+                                    2 -> stringResource(Res.string.card)
+                                    3 -> stringResource(Res.string.templates)
+                                    4 -> stringResource(Res.string.data)
+                                    5 -> stringResource(Res.string.security)
+                                    6 -> stringResource(Res.string.cowriter)
+                                    7 -> stringResource(Res.string.app_info)
+                                    else -> stringResource(Res.string.settings)
                                 }
+                        )
+                    },
+                    navigationIcon = {
+                        if (navigator.canNavigateBack()) {
+                            IconButton(
+                                shape = dialogShape(),
+                                onClick = { coroutineScope.launch { navigator.navigateBack() } }
+                            ) {
+                                Icon(
+                                    imageVector = if (currentPlatformInfo.operatingSystem == OS.IOS || currentPlatformInfo.operatingSystem == OS.MACOS)
+                                        Icons.Default.ArrowBackIosNew
+                                    else Icons.AutoMirrored.Filled.ArrowBack,
+                                    contentDescription = null
+                                )
                             }
+                        }
+                    },
+                    actions = {
+                        IconButton(
+                            shape = dialogShape(),
+                            onClick = { triggerExit() }
                         ) {
                             Icon(
-                                imageVector = if (currentPlatformInfo.operatingSystem == OS.IOS || currentPlatformInfo.operatingSystem == OS.MACOS)
-                                    Icons.Default.ArrowBackIosNew
-                                else Icons.AutoMirrored.Filled.ArrowBack,
+                                imageVector = Icons.Default.Close,
                                 contentDescription = null
                             )
                         }
                     }
-                    if (isExpanded) {
-                        Text(
-                            modifier = Modifier.align(Alignment.Center),
-                            text = stringResource(Res.string.settings),
-                            style = MaterialTheme.typography.titleMediumEmphasized
-                        )
-                    } else {
-                        Text(
-                            modifier = Modifier.align(Alignment.Center),
-                            style = MaterialTheme.typography.titleMediumEmphasized,
-                            text = when (selectedItem) {
-                                0 -> stringResource(Res.string.style)
-                                1 -> stringResource(Res.string.editor)
-                                2 -> stringResource(Res.string.card)
-                                3 -> stringResource(Res.string.templates)
-                                4 -> stringResource(Res.string.data)
-                                5 -> stringResource(Res.string.security)
-                                6 -> stringResource(Res.string.cowriter)
-                                7 -> stringResource(Res.string.app_info)
-                                else -> stringResource(Res.string.settings)
-                            }
-                        )
-                    }
-                    IconButton(
-                        modifier = Modifier.align(Alignment.CenterEnd),
-                        shape = dialogShape(),
-                        onClick = { triggerExit() }
-                    ) {
-                        Icon(
-                            imageVector = Icons.Default.Close,
-                            contentDescription = null
-                        )
-                    }
-                }
+                )
                 ListDetailPaneScaffold(
                     directive = navigator.scaffoldDirective,
                     value = navigator.scaffoldValue,
