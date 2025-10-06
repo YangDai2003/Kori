ece7513ad0994800daf667d3ac77781671096404
yang <dy15800837435@gmail.com>
2025-10-06 14:04:40 +0200

    Add Sonnet 4.5 and reorder Anthropic models

--- composeApp/src/commonMain/kotlin/knet/ai/providers/Anthropic.kt
+++ composeApp/src/commonMain/kotlin/knet/ai/providers/Anthropic.kt
@@ -6,12 +6,13 @@ object Anthropic {
     val anthropicModelMap = mapOf(
         AnthropicModels.Opus_3.id to AnthropicModels.Opus_3,
         AnthropicModels.Haiku_3.id to AnthropicModels.Haiku_3,
-        AnthropicModels.Sonnet_3_5.id to AnthropicModels.Sonnet_3_5,
         AnthropicModels.Haiku_3_5.id to AnthropicModels.Haiku_3_5,
+        AnthropicModels.Sonnet_3_5.id to AnthropicModels.Sonnet_3_5,
         AnthropicModels.Sonnet_3_7.id to AnthropicModels.Sonnet_3_7,
         AnthropicModels.Sonnet_4.id to AnthropicModels.Sonnet_4,
         AnthropicModels.Opus_4.id to AnthropicModels.Opus_4,
-        AnthropicModels.Opus_4_1.id to AnthropicModels.Opus_4_1
+        AnthropicModels.Opus_4_1.id to AnthropicModels.Opus_4_1,
+        AnthropicModels.Sonnet_4_5.id to AnthropicModels.Sonnet_4_5,
     )
 
     fun getModel(id: String) = anthropicModelMap[id] ?: anthropicModelMap.values.first()
