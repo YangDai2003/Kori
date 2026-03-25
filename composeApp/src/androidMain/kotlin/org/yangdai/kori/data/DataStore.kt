9739700aee96d6ce5901a8a1c503ef54ce237f92
yang <dy15800837435@gmail.com>
2026-03-25 19:40:52 +0800

    refactor(datastore): update Android DataStore implementation and Gradle version
    - Replace custom createDataStore function with Context.dataStore extension property
    - Use preferencesDataStore delegate for lazy initialization of DataStore instance
    - Update Gradle wrapper from version 9.3.1 to 9.4.1
    - Remove redundant createDataStore function in Android module
    - Update timestamp in gradle-wrapper.properties file

--- composeApp/src/androidMain/kotlin/org/yangdai/kori/data/DataStore.kt
+++ composeApp/src/androidMain/kotlin/org/yangdai/kori/data/DataStore.kt
@@ -3,9 +3,7 @@ package org.yangdai.kori.data
 import android.content.Context
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
-import org.yangdai.kori.data.local.createDataStore
+import androidx.datastore.preferences.preferencesDataStore
 import org.yangdai.kori.data.local.dataStoreFileName
 
-fun createDataStore(context: Context): DataStore<Preferences> = createDataStore(
-    producePath = { context.filesDir.resolve(dataStoreFileName).absolutePath }
-)
+val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = dataStoreFileName)
--- composeApp/src/androidMain/kotlin/org/yangdai/kori/data/di/AppModule.android.kt
+++ composeApp/src/androidMain/kotlin/org/yangdai/kori/data/di/AppModule.android.kt
@@ -5,12 +5,12 @@ import knet.ConnectivityObserver
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.module.Module
 import org.koin.dsl.module
-import org.yangdai.kori.data.createDataStore
+import org.yangdai.kori.data.dataStore
 import org.yangdai.kori.data.getDatabaseBuilder
 import org.yangdai.kori.data.local.getRoomDatabase
 
 actual fun platformModule(): Module = module {
     single { getRoomDatabase(getDatabaseBuilder(androidContext())) }
-    single { createDataStore(androidContext()) }
+    single { androidContext().dataStore }
     single<ConnectivityObserver> { AndroidConnectivityObserver(androidContext()) }
 }
